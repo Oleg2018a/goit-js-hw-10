7d57d7800f41e99e8d04894e3908592839af2078
Oleg2018a <126179919+Oleg2018a@users.noreply.github.com>
2023-08-03 17:44:28 +0300

    last fix commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+
 import { Notify } from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import { hideErrorMessage, showErrorMessage } from './js/error';
+import { showLoader, hideLoader } from './js/loader';
 
 const selectElem = document.querySelector('.breed-select');
+ const catImage = document.querySelector('.cat-image');
+ const catBreed = document.querySelector('#cat-breed');
+ const catDescription = document.querySelector('#cat-description');
+ const catTemperament = document.querySelector('#cat-temperament');
 
+function clearCatInfo() {
+  catImage.src = '';
+  catBreed.textContent = '';
+  catDescription.textContent = '';
+  catTemperament.textContent = '';
+}
 function populateBreedSelect(breeds) {
   const optionsHTML = breeds
     .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
@@ -12,33 +24,53 @@ function populateBreedSelect(breeds) {
   selectElem.innerHTML = optionsHTML;
 
   selectElem.addEventListener('change', () => {
+
     hideErrorMessage();
     const selectedBreedId = selectElem.value;
+    clearCatInfo();
+    
+    document.querySelector('.cat-info').classList.add('hidden');
+    showLoader();
 
     fetchCatByBreed(selectedBreedId)
+      .then(data => {
+        hideLoader();
+        document.querySelector('.cat-info').classList.remove('hidden');
+        return data[0];
+      })
       .then(cat => {
-        const catImage = document.querySelector('.cat-image');
-        const catBreed = document.querySelector('#cat-breed');
-        const catDescription = document.querySelector('#cat-description');
-        const catTemperament = document.querySelector('#cat-temperament');
-
         catImage.src = cat.url;
         catBreed.textContent = cat.breeds[0].name;
         catDescription.textContent = cat.breeds[0].description;
         catTemperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
       })
+
       .catch(() => {
+        hideLoader();
         showErrorMessage();
         Notify.failure('Помилка при отриманні інформації про породу');
+        clearCatInfo();
       });
+
   });
 }
+document.querySelector('.breed-select').classList.add('hidden');
+  showLoader();
 
 fetchBreeds()
+  .then(data => {
+    hideLoader();
+    document.querySelector('.breed-select').classList.remove('hidden');
+    return data;
+  })
   .then(breeds => {
     populateBreedSelect(breeds);
   })
+
   .catch(() => {
+    hideLoader();
     showErrorMessage();
     Notify.failure('Помилка при отриманні інформації про кота ');
+    clearCatInfo();
   });
+   
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,13 +1,11 @@
-import { hideLoader, showLoader } from './loader';
+
 
 const API_KEY =
   'live_oHrIS74pYNWjAiD38Hi3W0pSM7IJGybIGcSQ6CpezeUwseYH3a2AEvsBHPImN4tI';
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
-  document.querySelector('.breed-select').classList.add('hidden');
-  showLoader();
-
+ 
   return fetch(`${BASE_URL}/breeds`, {
     headers: {
       'x-api-key': API_KEY,
@@ -20,21 +18,11 @@ export function fetchBreeds() {
 
       return resp.json();
     })
-    .then(data => {
-      hideLoader();
-      document.querySelector('.breed-select').classList.remove('hidden');
-      return data;
-    })
-    .catch(error => {
-      hideLoader();
-      throw error;
-    });
+  
 }
 
 export function fetchCatByBreed(breedId) {
-  document.querySelector('.cat-info').classList.add('hidden');
-  showLoader();
-
+    
   return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`, {
     headers: {
       'x-api-key': API_KEY,
@@ -46,14 +34,5 @@ export function fetchCatByBreed(breedId) {
       }
       return resp.json();
     })
-    .then(data => {
-      hideLoader();
-      document.querySelector('.cat-info').classList.remove('hidden');
-      return data[0];
-    })
-    .catch(error => {
-      hideLoader();
-
-      throw error;
-    });
+  
 }
